3b837ab68c26156f95a9fbb7934663154d481dc0
Snehal Tilekar <snehaltilekar18@gmail.com>
2022-05-06 16:38:31 +0530

    Added employee database

---
+++ Employee.java
@@ -0,0 +1,35 @@
+package SampleJavaProgram;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		/* Employee Declaration */
+		String empName;
+		int empId;
+		int empAge;
+		String empMobNo;
+		double empSal;
+		String empAddress;
+		
+		/* Employee Initialization */
+		
+		empName = "SNEHAL";
+		empId = 741;
+		empAge = 26;
+		empMobNo = "9823733719";
+		empSal = 50000;
+		empAddress = "BHOSARI,PUNE-411039";
+		
+		/* Usage */
+		
+		System.out.println("Name of Employee is :"+empName);
+		System.out.println("Id of Employee is :"+empId);
+		System.out.println("Age of Employee is :"+empAge);
+		System.out.println("Contact_No of Employee is :"+empMobNo);
+		System.out.println("Salary of Employee is :"+empSal);
+		System.out.println("Address of Employee is :"+empAddress);
+		
+
+	}
+
+}
